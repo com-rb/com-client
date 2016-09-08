8549d49040f4a85e51ab165296b51eafbd1cc590
Zeke Fast <zekefast@gmail.com>
2016-09-08 14:35:53 +0200

    Add module structure of the library(gem).
    Changes:
    - Add follow modules:
      - ::Com::Client
      - ::Com::Client::Error
    - Add ::Com::Client::VERSION constant definition.

---
+++ lib/com/client.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "com/client/version"
+
+
+# Purpose: namespace
+#
+# Namespace for client modules and classes of com-client.
+module ::Com::Client
+end
+
+
+require "com/client/error"
